ac4c136f0ab4842a23407c4b5f8c11b12c8c3af4
Bijan Tabatabai <bijan311@gmail.com>
2021-10-01 14:09:37 -0500

    Turn anonymous memory into file backed memory
    We back the previously anonymous memory with a temorary file created in the kernel.
    To make sure we don't use anonymous CoW pages, change MAP_PRIVATE mapping to MAP_SHARED.
    
    This code currently leaves the files in place after the program exits, so we will need to delete them

--- include/linux/file_only_mem.h
+++ include/linux/file_only_mem.h
@@ -2,15 +2,22 @@
 #define _FILE_ONLY_MEM_H_
 
 #include <linux/types.h>
+#include <linux/fs.h>
 
 #ifdef CONFIG_FILE_ONLY_MEM
 
 bool use_file_only_mem(pid_t pid);
 
+struct file *create_new_fom_file(unsigned long len, unsigned long prot);
+
 #else //CONFIG_FILE_ONLY_MEM
 
 inline bool use_file_only_mem(pid_t pid) {
 	return false;
+}
+
+inline struct file *create_new_fom_file(unsigned long len, unsigned long prot) {
+	return NULL;
 }
 
 #endif //CONFIG_FILE_ONLY_MEM
