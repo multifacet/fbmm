13211397a1a50dd104bc9282769464a35279eddd
Bijan Tabatabai <bijan311@gmail.com>
2023-04-10 15:49:00 -0500

    Start working on a basic MMFS that only allocates base pages

---
+++ BasicMMFS/basic.c
@@ -0,0 +1,309 @@
+#include <linux/kernel.h>
+#include <linux/mm.h>
+#include <linux/mman.h>
+#include <linux/gfp.h>
+#include <linux/fs_context.h>
+#include <linux/fs_parser.h>
+#include <linux/pagemap.h>
+#include <linux/statfs.h>
+#include <linux/module.h>
+
+#include "basic.h"
+
+static const struct super_operations basicmmfs_ops;
+static const struct inode_operations basicmmfs_dir_inode_operations;
+
+struct basicmmfs_sb_info *BMMFS_SB(struct super_block *sb)
+{
+    return sb->s_fs_info;
+}
+
+struct basicmmfs_inode_info *BMMFS_I(struct inode *inode)
+{
+    return inode->i_private;
+}
+
+static vm_fault_t basicmmfs_fault(struct vm_fault *vmf)
+{
+    return VM_FAULT_OOM;
+}
+
+static struct vm_operations_struct basicmmfs_vm_ops = {
+    .fault = basicmmfs_fault,
+    .page_mkwrite = basicmmfs_fault,
+    .pfn_mkwrite = basicmmfs_fault,
+};
+
+static int basicmmfs_mmap(struct file *file, struct vm_area_struct *vma)
+{
+    file_accessed(file);
+    vma->vm_ops = &basicmmfs_vm_ops;
+    vma->vm_flags |= VM_MIXEDMAP | VM_HUGEPAGE;
+
+    return 0;
+}
+
+static long basicmmfs_fallocate(struct file *file, int mode, loff_t offset, loff_t len)
+{
+    return -EOPNOTSUPP;
+}
+
+const struct file_operations basicmmfs_file_operations = {
+    .mmap		= basicmmfs_mmap,
+    .mmap_supported_flags = MAP_SYNC,
+    .fsync		= noop_fsync,
+    .splice_read	= generic_file_splice_read,
+    .splice_write	= iter_file_splice_write,
+    .llseek		= generic_file_llseek,
+    .get_unmapped_area	= thp_get_unmapped_area,
+    .fallocate = basicmmfs_fallocate,
+};
+
+const struct inode_operations basicmmfs_file_inode_operations = {
+	.setattr	= simple_setattr,
+	.getattr	= simple_getattr,
+};
+
+const struct address_space_operations basicmmfs_aops = {
+    .direct_IO = noop_direct_IO,
+    .dirty_folio = noop_dirty_folio,
+};
+
+struct inode *basicmmfs_get_inode(struct super_block *sb,
+                const struct inode *dir, umode_t mode, dev_t dev)
+{
+    struct inode *inode = new_inode(sb);
+    struct basicmmfs_inode_info *info;
+
+    if (!inode)
+        return NULL;
+
+    info = kzalloc(sizeof(struct basicmmfs_inode_info), GFP_KERNEL);
+    if (!info) {
+        pr_err("BasicMMFS: Failure allocating inode");
+        return NULL;
+    }
+    mt_init(&info->mt);
+
+    inode->i_ino = get_next_ino();
+    inode_init_owner(&init_user_ns, inode, dir, mode);
+    inode->i_mapping->a_ops = &basicmmfs_aops;
+    inode->i_atime = inode->i_mtime = inode->i_ctime = current_time(inode);
+    inode->i_flags |= S_DAX;
+    inode->i_private = info;
+    switch (mode & S_IFMT) {
+        case S_IFREG:
+            inode->i_op = &basicmmfs_file_inode_operations;
+            inode->i_fop = &basicmmfs_file_operations;
+            break;
+        case S_IFDIR:
+            inode->i_op = &basicmmfs_dir_inode_operations;
+            inode->i_fop = &simple_dir_operations;
+
+            /* Directory inodes start off with i_nlink == 2 (for "." entry) */
+            inc_nlink(inode);
+            break;
+        default:
+            return NULL;
+    }
+
+    return inode;
+}
+
+static int basicmmfs_mknod(struct user_namespace *mnt_userns, struct inode *dir,
+            struct dentry *dentry, umode_t mode, dev_t dev)
+{
+    struct inode *inode = basicmmfs_get_inode(dir->i_sb, dir, mode, dev);
+    int error = -ENOSPC;
+
+    if (inode) {
+        d_instantiate(dentry, inode);
+        dget(dentry); /* Extra count - pin the dentry in core */
+        error = 0;
+        dir->i_mtime = dir->i_ctime = current_time(dir);
+    }
+
+    return error;
+}
+
+static int basicmmfs_mkdir(struct user_namespace *mnt_userns, struct inode *dir,
+            struct dentry *dentry, umode_t mode)
+{
+    return -EINVAL;
+}
+
+static int basicmmfs_create(struct user_namespace *mnt_userns, struct inode *dir,
+            struct dentry *dentry, umode_t mode, bool excl)
+{
+    // TODO: Replace 0777 with mode and see if anything breaks
+    return basicmmfs_mknod(&init_user_ns, dir, dentry, 0777 | S_IFREG, 0);
+}
+
+static int basicmmfs_symlink(struct user_namespace *mnt_userns, struct inode *dir,
+            struct dentry *dentry, const char *symname)
+{
+    return -EINVAL;
+}
+
+static int basicmmfs_tmpfile(struct user_namespace *mnt_userns,
+            struct inode *dir, struct file *file, umode_t mode)
+{
+    struct inode *inode;
+
+    inode = basicmmfs_get_inode(dir->i_sb, dir, mode, 0);
+    if (!inode)
+        return -ENOSPC;
+    d_tmpfile(file, inode);
+    return 0;
+}
+
+static const struct inode_operations basicmmfs_dir_inode_operations = {
+    .create     = basicmmfs_create,
+    .lookup     = simple_lookup,
+    .link       = simple_link,
+    .unlink     = simple_unlink,
+    .symlink    = basicmmfs_symlink,
+    .mkdir      = basicmmfs_mkdir,
+    .rmdir      = simple_rmdir,
+    .mknod      = basicmmfs_mknod,
+    .rename     = simple_rename,
+    .tmpfile    = basicmmfs_tmpfile,
+};
+
+static int basicmmfs_statfs(struct dentry *dentry, struct kstatfs *buf)
+{
+    struct super_block *sb = dentry->d_sb;
+    struct basicmmfs_sb_info *sbi = BMMFS_SB(sb);
+
+    buf->f_type = sb->s_magic;
+    buf->f_bsize = PAGE_SIZE;
+    buf->f_blocks = sbi->num_pages;
+    buf->f_bfree = buf->f_bavail = sbi->free_pages;
+    buf->f_files = LONG_MAX;
+    buf->f_ffree = LONG_MAX;
+    buf->f_namelen = 255;
+
+    return 0;
+}
+
+static void basicmmfs_free_inode(struct inode *inode)
+{
+    struct basicmmfs_inode_info *inode_info = BMMFS_I(inode);
+
+    mtree_destroy(&inode_info->mt);
+    kfree(inode_info);
+}
+
+static int basicmmfs_show_options(struct seq_file *m, struct dentry *root)
+{
+    return 0;
+}
+
+static const struct super_operations basicmmfs_ops = {
+    .statfs = basicmmfs_statfs,
+    .free_inode = basicmmfs_free_inode,
+    .drop_inode = generic_delete_inode,
+    .show_options = basicmmfs_show_options,
+};
+
+static int basicmmfs_fill_super(struct super_block *sb, struct fs_context *fc)
+{
+    struct inode *inode;
+    struct basicmmfs_sb_info *sbi = kzalloc(sizeof(struct basicmmfs_sb_info), GFP_KERNEL);
+
+    if (!sbi) {
+        return -ENOMEM;
+    }
+
+    sb->s_fs_info = sbi;
+    sb->s_maxbytes = MAX_LFS_FILESIZE;
+    sb->s_magic = 0xDEADBEEF;
+    sb->s_op = &basicmmfs_ops;
+    sb->s_time_gran = 1;
+    if (!sb_set_blocksize(sb, PAGE_SIZE)) {
+        pr_err("BasicMMFS: error setting blocksize");
+        kfree(sbi);
+    }
+
+    INIT_LIST_HEAD(&sbi->free_list);
+    INIT_LIST_HEAD(&sbi->active_list);
+    // TODO: Get the number of pages to request from a mount arg
+    // Might need to be GFP_HIGHUSER?
+    sbi->num_pages = alloc_pages_bulk_list(GFP_USER, 1024, &sbi->free_list);
+    sbi->free_pages = sbi->num_pages;
+
+    inode = basicmmfs_get_inode(sb, NULL, S_IFDIR | 0777, 0);
+    sb->s_root = d_make_root(inode);
+    if (!sb->s_root) {
+        kfree(sbi);
+        return -ENOMEM;
+    }
+
+    return 0;
+}
+
+static int basicmmfs_get_tree(struct fs_context *fc)
+{
+    return get_tree_bdev(fc, basicmmfs_fill_super);
+}
+
+enum basicmmfs_param {
+    Opt_maxsize,
+};
+
+const struct fs_parameter_spec basicmmfs_fs_parameters[] = {
+    fsparam_u64("maxsize", Opt_maxsize),
+};
+
+static int basicmmfs_parse_param(struct fs_context *fc, struct fs_parameter *param)
+{
+    return 0;
+}
+
+static void basicmmfs_free_fc(struct fs_context *fc)
+{
+}
+
+static const struct fs_context_operations basicmmfs_context_ops = {
+    .free = basicmmfs_free_fc,
+    .parse_param = basicmmfs_parse_param,
+    .get_tree = basicmmfs_get_tree,
+};
+
+int basicmmfs_init_fs_context(struct fs_context *fc)
+{
+    fc->ops = &basicmmfs_context_ops;
+
+    return 0;
+}
+
+static void basicmmfs_kill_sb(struct super_block *sb)
+{
+    kill_litter_super(sb);
+}
+
+static struct file_system_type basicmmfs_fs_type = {
+    .owner = THIS_MODULE,
+    .name = "BasicMMFS",
+    .init_fs_context = basicmmfs_init_fs_context,
+    .parameters = basicmmfs_fs_parameters,
+    .kill_sb = basicmmfs_kill_sb,
+    .fs_flags = FS_USERNS_MOUNT,
+};
+
+int __init init_module(void)
+{
+    printk(KERN_INFO "Starting BasicMMFS");
+    register_filesystem(&basicmmfs_fs_type);
+
+    return 0;
+}
+
+void cleanup_module(void)
+{
+    printk(KERN_INFO "Removing BasicMMFS");
+    unregister_filesystem(&basicmmfs_fs_type);
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Bijan Tabatabai");
---
+++ BasicMMFS/basic.h
@@ -0,0 +1,22 @@
+#ifndef BASIC_MMFS_H
+#define BASIC_MMFS_H
+
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/maple_tree.h>
+#include <linux/spinlock.h>
+#include <linux/sched.h>
+
+struct basicmmfs_sb_info {
+    struct list_head free_list;
+    struct list_head active_list;
+    u64 num_pages;
+    u64 free_pages;
+};
+
+struct basicmmfs_inode_info {
+    // Maple tree mapping the page offset to the folio mapped to that offset
+    struct maple_tree mt;
+};
+
+#endif //BASIC_MMFS_H
