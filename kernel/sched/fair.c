aa1f6e300bd89a03c52c9e0bc426622500a61f9f
BIJAN TABATABAI <btabatabai@wisc.edu>
2023-09-18 14:36:16 -0500

    Make numa balancing scan period min modifyable

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -227,6 +227,14 @@ static struct ctl_table sched_fair_sysctls[] = {
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
 	},
+	{
+		.procname	= "numa_balancing_scan_period_min_ms",
+		.data		= &sysctl_numa_balancing_scan_period_min,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+	},
 #endif /* CONFIG_NUMA_BALANCING */
 	{}
 };
