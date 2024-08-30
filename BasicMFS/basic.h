1d2dc85ae7f15286be254b1bac1f74df10eaacaa
Bijan Tabatabai <btabatabai@wisc.edu>
2024-08-30 12:23:06 -0500

    Add base implementation of an MFS
    Mount by running
    sudo mount -t BasicMFS BasicMFS -o numpages=<pages> <mntdir>
    
    Where <pages> is the max number of 4KB pages it can use, and <mntdir> is
    the directory to mount the filesystem to.
    
    This patch is meant to serve as a reference for the reviewers and is not
    intended to be upstreamed.
    
    Signed-off-by: Bijan Tabatabai <btabatabai@wisc.edu>

---
+++ BasicMFS/basic.h
@@ -0,0 +1,29 @@
+#ifndef BASIC_MMFS_H
+#define BASIC_MMFS_H
+
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/maple_tree.h>
+#include <linux/spinlock.h>
+#include <linux/sched.h>
+
+struct basicmfs_sb_info {
+	spinlock_t lock;
+	struct list_head free_list;
+	struct list_head active_list;
+	u64 num_pages;
+	u64 max_pages;
+	u64 free_pages;
+};
+
+struct basicmfs_inode_info {
+	// Maple tree mapping the page offset to the folio mapped to that offset
+	// Used to hold preallocated pages that haven't been mapped yet
+	struct maple_tree falloc_mt;
+	// The first virtual address this file is associated with.
+	u64 file_va_start;
+	// The file offset to folio mapping from the file
+	struct address_space *mapping;
+};
+
+#endif //BASIC_MMFS_H
